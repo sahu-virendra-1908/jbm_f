560bb25fbd591e3380f286d2a4c03c8c12dc5865
sahu-virendra-1908 <virendrashivsahu@gmail.com>
2026-03-15 10:48:32 +0530

    h

--- src/api/gemini.js
+++ src/api/gemini.js
@@ -1,49 +1,7 @@
-// import axios from "axios"
-
-// const API_KEY = "AIzaSyCABaj3GVC1OY-GgHLIkhkyWZHU8gRig6Q"
-
-// export const analyzeCase = async (problem) => {
-
-//   const prompt = `
-// You are an expert legal and investigation research AI.
-
-// User problem:
-// ${problem}
-
-// Tasks:
-// 1. Understand the problem deeply
-// 2. Verify the context logically
-// 3. Research possible laws or actions
-// 4. Give a clear step-by-step action plan
-// 5. Explain risks and precautions
-// 6. Respond in the user's native language
-
-// Return format:
-
-// Problem Understanding
-// Verification
-// Research Findings
-// Step-by-step Plan
-// Important Advice
-// `
-
-//   const res = await axios.post(
-//     `https://generativelanguage.googleapis.com/v1beta/models/gemini-pro:generateContent?key=${API_KEY}`,
-//     {
-//       contents: [
-//         {
-//           parts: [{ text: prompt }]
-//         }
-//       ]
-//     }
-//   )
-
-//   return res.data.candidates[0].content.parts[0].text
-// }
 
 import axios from "axios"
 
-const API_KEY = "AIzaSyB-leD1AT1wwFSj4aEPGD8AP-cia4wu6wY"
+import.meta.env.VITE_GEMINI_API_KEY
 
 export const analyzeCase = async (problem) => {
 
